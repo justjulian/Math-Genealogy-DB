fc0379b89f02799a45cbffee94febc16bb98a03c
justjulian <justjulian@arcor.de>
2011-07-31 02:05:13 +0000

    upgraded to Python3

--- mgdb.py
+++ mgdb.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
+
 import mathgenealogy
 
 
 
 mgdb = mathgenealogy.Mathgenealogy()
+
 try:
     mgdb.parseInput()
-except SyntaxError, e:
-    print mgdb.parser.get_usage()
-    print e
+except SyntaxError as e:
+    print(e)
+    print(mgdb.parser.get_usage())
